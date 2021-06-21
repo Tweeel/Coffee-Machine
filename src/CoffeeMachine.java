6213c463e92715cb0757d4780e2b33d378ff27b9
houssem eddine <75279465+Tweeel@users.noreply.github.com>
2021-06-21 22:59:15 +0100

    Update CoffeeMachine.java

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -99,53 +99,6 @@ public class CoffeeMachine {
         }
     }
 
-    public static void afficher_menu(){
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
-    }
-
-    public static void afficher_needed(int cups, int water, int milk, int coffee_beans){
-        System.out.println("For "+cups+" cups of coffee you will need:");
-        System.out.println(water+" ml of water");
-        System.out.println(milk+" ml of milk");
-        System.out.println(coffee_beans+" g of coffee beans");
-    }
-
-    public static void request_coffee(){
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water_have=scanner.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk_have=scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffee_beans_have=scanner.nextInt();
-        System.out.println("Write how many cups of coffee you will need: ");
-        int cups=scanner.nextInt();
-        int water_needed=cups*200;
-        int milk_needed=cups*50;
-        int coffee_beans_needed=cups*15;
-
-        int water_result=water_have/200;
-        int milk_result=milk_have/50;
-        int coffee_beans_result=coffee_beans_have/15;
-        int min = Math.min(Math.min( coffee_beans_result, milk_result),water_result );
-
-        if(min<cups){
-            System.out.println("No, I can make only "+ min +" cup(s) of coffee");
-        }
-        else if (min==cups){
-            System.out.println("Yes, I can make that amount of coffee");
-        }
-        else{
-            System.out.println("Yes, I can make that amount of coffee (and even "+ (min-cups)+" more than that)");
-        }
-    }
-
     public static void afficher_ingredients(int water,int milk,int coffee_beans,int cups,int money){
         System.out.println("The coffee machine has:");
         System.out.println(water+" ml of water");
